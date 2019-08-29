99be4e3dd09b6e54d559ca267023f93efe2dfd72
DuyN <duy210697@gmail.com>
2019-08-29 23:48:12 +0700

    change cmd ffmpeg

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const spawn = require('child_process').spawn;
 
-const tempFilePath = './video/sample.mp4';
+const tempFilePath = './video/Avengers.Endgame.2019.Vsub.1080p.BrRip.6CH.x265.HEVC-PSA.mkv';
 const convertedFile = 'converted.mp4';
 
 // ffmpeg -i h264.mp4 -c:v copy -bsf:v h264_mp4toannexb -an out.h264
-const ffmpeg = spawn('ffmpeg', ['-i', tempFilePath, '-c:v', 'copy', '-bsf:v', 'h264_mp4toannexb', '-an', convertedFile]);
+const ffmpeg = spawn('ffmpeg', [
+    '-i',
+    tempFilePath,
+    '-c:v',
+    'copy',
+    '-bsf:v',
+    'h264_mp4toannexb',
+    '-an',
+    '-b:v',
+    '5M',
+    '-framerate',
+    '30',
+    convertedFile
+]);
 
+// const cmd = `ffmpeg -i ${tempFilePath} -aspect 16:9 -vf scale=-2:720 -c:v libx264 -crf 0 -preset veryslow -c:a copy ${convertedFile}`;
+// const ffmpeg = spawn('ffmpeg', cmd.split(' '));
+
+// console.log(cmd);
+
+const begin = Date.now();
 ffmpeg.on('exit', (statusCode) => {
     if (statusCode === 0) {
+        console.log('conversion', Date.now() - begin);
         console.log('conversion successfully');
     }
 });
 
 ffmpeg.stderr.on('data', (error) => {
-    console.error(error);
+    console.error(error.toString());
 });
--- test-handbrake.js
+++ test-handbrake.js
@@ -1,10 +1,10 @@
-const hbjs = require('handbrake-js')
- 
+const hbjs = require('handbrake-js');
+
 hbjs.spawn({
-    input: 'video/sample.mp4',
-    output: 'something.mp4',
-    // preset: 'Very Fast 720p30',
-    preset: 'Very Fast 480p30',
+  input: 'video/sample.mp4',
+  output: 'something.mp4',
+  // preset: 'Very Fast 720p30',
+  preset: 'Very Fast 480p30',
 })
   .on('error', err => {
     // invalid user input, no video found etc
@@ -17,3 +17,6 @@ hbjs.spawn({
       progress.eta
     )
   })
+  .on('end', () => {
+    console.log('conversion success');
+  });
